32842440f4cdb40ec23dbbc1979d75cc5f86f46b
Drz103 <88671231+Drz103@users.noreply.github.com>
2021-11-19 07:37:36 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fet
 ├ PULSA : 083813679823
 ├ DANA : 085714313795
 └────
-`.trim(), '©RadBitZ', 'Owner', '.owner')
+`.trim(), '©RadBotZ', 'SEWA', '.sewabot')
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
